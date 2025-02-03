aab67587e913e3655f10bff85be0e99a497490b2
7account <DAvidliancongc2789@nycstudents.net>
2025-02-03 14:23:57 +0000

    (a) complete (all test cases and methods)

--- Game.java
+++ Game.java
@@ -2,13 +2,27 @@ public class Game {
     private Level levelOne;
     private Level levelTwo;
     private Level levelThree;
+    private boolean bonus;
 
     public Game() {
-        
+        levelOne = new Level();
+        levelTwo = new Level();
+        levelThree = new Level();
     }
     
     public boolean isBonus() {
+        return bonus;
+    }
 
+    public void makeBonus() {
+        bonus = true;
+    }
+
+    public Level getLevel(int x) {
+        if (x == 1) return levelOne;
+        if (x == 2) return levelTwo;
+        if (x == 3) return levelThree;
+        return null;
     }
 
     public void play() {
@@ -16,10 +30,21 @@ public class Game {
     }
 
     public int getScore() {
-
+        int points = 0;
+        if (levelOne.goalReached()) {
+            points += levelOne.getPoints();
+            if (levelTwo.goalReached()) {
+                points += levelTwo.getPoints();
+                if (levelThree.goalReached()) {
+                    points += levelThree.getPoints();
+                    if (isBonus()) points *= 3;
+                }
+            }
+        }
+        return points;
     }
 
     public int playManyTimes() {
-
+        return 0;
     }
 }
